f17a221e09631c31558827097ef2c2b3b6071ec3
Luis Rojas <lrojasjimenez@goodgamestudios.com>
2020-11-06 19:38:58 +0100

    New eatable feature

---
+++ Assets/Scripts/EatScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class EatScript : MonoBehaviour {
+    private void OnCollisionEnter(Collision other) {
+        var eatable = other.gameObject.GetComponent<Eatable>();
+        if (eatable) {
+            transform.localScale += new Vector3(eatable.Size, eatable.Size, eatable.Size);
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Eatable.cs
@@ -0,0 +1,5 @@
+﻿using UnityEngine;
+
+public class Eatable : MonoBehaviour {
+    public float Size = 1.0f;
+}
