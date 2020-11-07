b1a89c59e5846fb04a38ab542bd399bf2ce64076
Luis Rojas <lrojasjimenez@goodgamestudios.com>
2020-11-08 00:26:43 +0100

    Player controller split

---
+++ Assets/Scripts/Follower.cs
@@ -0,0 +1,31 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+public class Follower : MonoBehaviour {
+    [SerializeField] private Transform _sphere;
+    [SerializeField] private Transform _follower;
+    [SerializeField] private CinemachineVirtualCamera _virtualCamera;
+    
+    [SerializeField] private float _cameraDistanceFactor = 5.0f;
+    
+    private Vector3 _lastPosition;
+    private Cinemachine3rdPersonFollow _thirdPersonFollow;
+
+    private void Awake() {
+        CinemachineComponentBase componentBase = _virtualCamera.GetCinemachineComponent(CinemachineCore.Stage.Body);
+        if (componentBase is Cinemachine3rdPersonFollow) {
+            _thirdPersonFollow = componentBase as Cinemachine3rdPersonFollow;
+        }
+    }
+
+    private void Update() {
+        _thirdPersonFollow.CameraDistance = _sphere.transform.localScale.x * _cameraDistanceFactor;
+        var spherePosition = _sphere.position;
+        var direction = spherePosition - _lastPosition;
+        var targetPosition = spherePosition + direction;
+        targetPosition.y = _follower.position.y;
+        _follower.LookAt(targetPosition);
+        _follower.position = spherePosition;
+        _lastPosition = spherePosition;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,25 +2,25 @@
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    [SerializeField] private Transform _sphere;
-    [SerializeField] private Transform _follower;
     [SerializeField] private CinemachineVirtualCamera _virtualCamera;
     
     [SerializeField] private Rigidbody _rigidbody;
     [SerializeField] private float _strength = 1.0f;
     [SerializeField] private ForceMode _forceMode = ForceMode.Force;
-    [SerializeField] private float _cameraDistanceFactor = 5.0f;
     
     private Vector3 _lastPosition;
-    private Cinemachine3rdPersonFollow _thirdPersonFollow;
     private Transform _virtualCameraTransform;
 
     private void Awake() {
-        CinemachineComponentBase componentBase = _virtualCamera.GetCinemachineComponent(CinemachineCore.Stage.Body);
         _virtualCameraTransform = _virtualCamera.transform;
-        if (componentBase is Cinemachine3rdPersonFollow) {
-            _thirdPersonFollow = componentBase as Cinemachine3rdPersonFollow;
-        }
+    }
+
+    private void OnEnable() {
+        _rigidbody.useGravity = true;
+    }
+    
+    private void OnDisable() {
+        _rigidbody.useGravity = false;
     }
 
     private void Update() {
@@ -31,14 +31,5 @@ public class PlayerController : MonoBehaviour {
             var forward = _virtualCameraTransform.forward * (vertical * _strength * Time.deltaTime);
             _rigidbody.AddForce(forward + right, _forceMode);
         }
-
-        _thirdPersonFollow.CameraDistance = _sphere.transform.localScale.x * _cameraDistanceFactor;
-        var spherePosition = _sphere.position;
-        var direction = spherePosition - _lastPosition;
-        var targetPosition = spherePosition + direction;
-        targetPosition.y = _follower.position.y;
-        _follower.LookAt(targetPosition);
-        _follower.position = spherePosition;
-        _lastPosition = spherePosition;
     }
 }
