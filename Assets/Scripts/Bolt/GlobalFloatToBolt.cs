c17ad0db0f6bcc9665d7bd0c91dadfe89ffdf52f
Luis Rojas <lrojasjimenez@goodgamestudios.com>
2020-11-07 16:32:32 +0100

    Global float to bolt event on changed

---
+++ Assets/Scripts/Bolt/GlobalFloatToBolt.cs
@@ -0,0 +1,20 @@
+﻿using Bolt;
+using UnityEngine;
+
+namespace HalfBlind.ScriptableVariables {
+    public class GlobalFloatToBolt : MonoBehaviour {
+        [SerializeField] private GlobalFloat _float;
+
+        private void Awake() {
+            _float.OnTValueChanged += OnChanged;
+        }
+
+        private void OnChanged(float value) {
+            CustomEvent.Trigger(gameObject, _float.name, value);
+        }
+
+        private void OnDestroy() {
+            _float.OnTValueChanged -= OnChanged;
+        }
+    }
+}
