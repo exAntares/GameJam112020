184ba44b37bd7dbcd8dfac74170558580e473ffd
Luis Rojas <lrojasjimenez@goodgamestudios.com>
2020-11-08 16:26:57 +0100

    Bug on world

---
+++ Assets/Scripts/Grounded.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Grounded : MonoBehaviour {
+    [SerializeField] private Transform _bug;
+    
+    void Update() {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, float.MaxValue)) {
+            _bug.position = hit.point;
+        }
+    }
+}
